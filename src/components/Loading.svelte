b42c6a831a018bc35184fcd50290a0aa473a58a3
Haydon Lam <lamhaydon@gmail.com>
2020-09-12 15:56:51 +0100

    Loading component

---
+++ src/components/Loading.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Loading } from "attractions";
+  export let full: boolean;
+</script>
+
+<style>
+  :global(.pando_loading) {
+    padding: var(--paddingXL);
+  }
+  :global(.pando_loading.full) {
+    height: 100%;
+    width: 100%;
+  }
+</style>
+
+<Loading class="pando_loading {full && 'full'}" />
