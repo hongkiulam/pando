1a398609c2e624250af3e950bc7cb7a0be7d6d8e
Haydon Lam <lamhaydon@gmail.com>
2020-09-12 17:43:13 +0100

    Custom Input component

---
+++ src/components/Input.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { TextField } from "attractions";
+  export let value: string | number;
+  export let error: string;
+  export let withItem: boolean;
+  export let itemRight: boolean;
+  export let type: string;
+  export let outline: boolean;
+  export let label: string = "";
+  export let autoFocus: boolean;
+
+  let isFocussed = false;
+</script>
+
+<style>
+  .input_item {
+    top: 0 !important;
+    line-height: 2.8rem;
+  }
+  .input {
+    margin-bottom: var(--padding);
+  }
+  .isFocussed {
+    color: var(--primary);
+  }
+</style>
+
+<div class="input">
+  <span class:isFocussed>{label}</span>
+  <TextField
+    bind:value
+    events={[{ name: 'focus', handler: () => {
+          isFocussed = true;
+        } }, { name: 'blur', handler: () => {
+          isFocussed = false;
+        } }]}
+    {type}
+    {withItem}
+    {itemRight}
+    {error}
+    {outline}
+    {autoFocus}
+    {...$$restProps}>
+    <span class="item input_item">
+      <slot />
+    </span>
+  </TextField>
+</div>
