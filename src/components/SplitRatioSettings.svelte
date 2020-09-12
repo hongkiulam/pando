4a172a99a85c7a622f88b49fd78856aabdd30b22
Haydon Lam <lamhaydon@gmail.com>
2020-09-12 17:44:08 +0100

    Added inputs to split ratio

--- src/components/SplitRatioSettings.svelte
+++ src/components/SplitRatioSettings.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
-  import { TextField } from "attractions";
+  import Input from "../components/Input.svelte";
+  import Button from "attractions/button";
   import { db } from "../firebase";
-  $: splitRatio = $db?.splitRatio;
+
+  let toSaving = $db.splitRatio;
+  $: toSpending = 100 - toSaving;
+
+  $: validationError = toSaving < 1 || toSaving > 99;
+  $: isDirty = $db.splitRatio !== toSaving;
 </script>
 
-{#if splitRatio !== undefined}
-  <TextField />
-  {splitRatio}:{100 - splitRatio}
+<Input
+  type="number"
+  label="To Saving"
+  withItem
+  itemRight
+  bind:value={toSaving}
+  error={validationError && 'Value must be between 1 and 99'}>
+  %
+</Input>
+<Input
+  type="number"
+  disabled
+  label="To Spending"
+  withItem
+  itemRight
+  bind:value={toSpending}>
+  %
+</Input>
+{#if isDirty}
+  <Button outline>Save</Button>
 {/if}
