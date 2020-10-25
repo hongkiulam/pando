6e7965c744e9d4c176dcc66a53518ca36687a749
Haydon Lam <lamhaydon@gmail.com>
2020-10-25 16:29:00 +0000

    Add cards to home page

---
+++ src/components/Card.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import { fly } from "svelte/transition";
+
+  export let data: { title: string; content: string }[] = [];
+  export let small: boolean = false;
+  export let fancy: boolean = false;
+
+  const cardAnimationOptions = {
+    x: -20,
+    duration: 5000, // we will override this duration, setting to to 10 seconds so that svelte wont remove the animation too soon
+  };
+</script>
+
+<style lang="scss">
+  @mixin staggerAnimation {
+    @for $i from 1 through 10 {
+      &:nth-of-type(#{$i}) {
+        animation-delay: calc(#{$i - 2} * 100ms) !important;
+      }
+    }
+  }
+  .card {
+    background: var(--primary-light);
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+    border-radius: 20px;
+    padding: var(--padding);
+    width: 100%;
+    animation-duration: 400ms !important;
+    @include staggerAnimation();
+  }
+  .card_title {
+    color: var(--medium);
+  }
+  .small {
+    width: calc(50% - (var(--padding) / 2));
+  }
+  .fancy {
+    color: var(--primary-light);
+    background: var(--primary);
+    // box-shadow: 0 4px 12px var(--primary);
+    position: relative;
+    overflow: hidden;
+    // background: linear-gradient(90deg, var(--primary) 0%, var(--dark) 500%);
+
+    &::after {
+      content: "";
+      display: block;
+      width: 150%;
+      height: 150%;
+      background: linear-gradient(338deg, white, transparent);
+      opacity: 0.6;
+      right: -103px;
+      top: -9px;
+      position: absolute;
+      transform: rotate(-27deg);
+      transform-origin: top right;
+    }
+    .card_title {
+      color: inherit;
+    }
+  }
+</style>
+
+<div class="card" class:small class:fancy in:fly={cardAnimationOptions}>
+  {#each data as { title, content }}
+    <span class="card_title">{title}</span>
+    {#if fancy}
+      <h1>{content}</h1>
+    {:else}
+      <h2>{content}</h2>
+    {/if}
+  {/each}
+</div>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,7 +1,51 @@
-<script>
+<script lang="ts">
   import { db } from "../firebase";
   import { url } from "../store";
+  import Card from "../components/Card.svelte";
+  import { formatCurrency } from "../utils/formatCurrency";
   $url = "/";
+  $: finances = $db ? $db.finance : [];
+  $: stocks = $db ? $db.stocks : [];
+  $: totalInvested = stocks.reduce((acc, curr) => acc + curr.amount, 0);
+
+  $: earnedToDate = () => {
+    const earned = finances.reduce((acc, curr) => {
+      const thisMonthsIncome = curr.income.reduce((a, c) => a + c.amount, 0);
+      return acc + thisMonthsIncome;
+    }, 0);
+    return earned;
+  };
+
+  const daysUntilNextPayment = () => {
+    const today = new Date();
+    const firstOfNext = new Date(today.getMonth() + 1);
+    firstOfNext.setDate(0);
+    return firstOfNext.getDate() - today.getDate() - 1;
+  };
 </script>
 
-<pre>{JSON.stringify($db, null, 2)}</pre>
+<style lang="scss">
+  .container {
+    display: flex;
+    flex-flow: row wrap;
+    gap: var(--padding);
+    padding: var(--padding);
+  }
+</style>
+
+<!-- <pre>{JSON.stringify($db, null, 2)}</pre> -->
+<div class="container">
+  <Card
+    data={[{ title: 'Total saved', content: formatCurrency(finances[0].accSaved) }, { title: 'Allocated spending', content: formatCurrency(finances[0].accSpending) }]}
+    fancy />
+  <Card
+    data={[{ title: 'Next payment in', content: daysUntilNextPayment().toString() + ' days' }]}
+    small />
+  <Card
+    data={[{ title: 'Total invested', content: formatCurrency(totalInvested) }]}
+    small />
+  <Card
+    data={[{ title: 'Earned to date', content: formatCurrency(earnedToDate()) }]}
+    small />
+
+</div>
