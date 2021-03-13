87303962b94eb2466124b9a34f21c22f05ff623b
Haydon Lam <lamhaydon@gmail.com>
2021-03-13 17:30:05 +0000

    Fetch vanguard data from firestore when possible

--- src/actions/vanguard.ts
+++ src/actions/vanguard.ts
@@ -1,3 +1,4 @@
+import { firestore } from "../firebase";
 import { vanguard } from "../store";
 import type { Resource } from "../types/vanguard";
 
@@ -19,6 +20,40 @@ export const get = async (resource: Resource) => {
     ...currentState,
     loading: true,
   });
+
+  const vanguardDataDocRef = await firestore()
+    .collection("vanguard")
+    .doc(resource)
+    .get();
+  const vanguardDataFromFirestore = vanguardDataDocRef.data();
+  if (vanguardDataFromFirestore) {
+    const now = new Date();
+    const storedPerformanceDate = new Date(vanguardDataFromFirestore.date);
+    const isSameDay = now.getDate() === storedPerformanceDate.getDate();
+    if (isSameDay) {
+      const {
+        value,
+        amountChange,
+        percentageChange,
+      } = vanguardDataFromFirestore;
+
+      vanguard.update((state) => ({
+        ...state,
+        loading: false,
+        error: "",
+        data: {
+          ...state.data,
+          performance: {
+            value: Number(value),
+            amountChange: Number(amountChange),
+            percentageChange: Number(percentageChange),
+          },
+        },
+      }));
+      return;
+    }
+  }
+
   const response = await fetch(`${api}/${resource}`, {
     headers: {
       Authorization: `Basic ${btoa(username + ":" + password)}`,
